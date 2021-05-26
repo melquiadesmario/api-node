6b40ab70e1dd6e395d4281b06a68fc320fcbf3ba
melquiadesmario <melquiadesm@gmail.com>
2021-05-25 23:44:54 -0300

    Add Response Json

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/projects', (request, response) => {
-    return response.send('Hello World!');
+    return response.json({
+        message: 'Hello World!'
+    });
 });
 
 app.listen(3333);
