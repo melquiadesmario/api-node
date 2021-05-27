89a3bd023b6a827892efdb28f944a95dab82317d
melquiadesmario <melquiadesm@gmail.com>
2021-05-26 23:19:09 -0300

    First Contact with HTTP

--- src/index.js
+++ src/index.js
@@ -3,9 +3,37 @@ const express = require('express');
 const app = express();
 
 app.get('/projects', (request, response) => {
-    return response.json({
-        message: 'Hello World!'
-    });
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ]);
+});
+
+app.put('/projects', (request, response) => {
+    return response.json([
+        'Projeto 5',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ]);
+});
+
+app.delete('/projects', (resquest, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 4'
+    ]);
 });
 
 app.listen(3333, () => console.log('Backend started!'));
