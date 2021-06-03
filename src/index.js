26210cbd4ed85f2c75ef86ac8773621d713bad68
melquiadesmario <melquiadesm@gmail.com>
2021-06-02 22:57:53 -0300

    Filter Query Params

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ app.use(express.json());
 const projects = [];
 
 app.get('/projects', (request, response) => {
-    return response.json(projects);
+    const { title } = request.query;
+
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
+
+    return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
@@ -47,7 +53,7 @@ app.put('/projects/:id', (request, response) => {
     return response.json(project);
 });
 
-app.delete('/projects', (resquest, response) => {
+app.delete('/projects/:id', (request, response) => {
     const { id } = request.params;
 
     const projectIndex = projects.findIndex(project => project.id === id);
@@ -57,6 +63,7 @@ app.delete('/projects', (resquest, response) => {
     }
 
     projects.splice(projectIndex, 1);
+    
     return response.status(204).json([]);
 });
 
