fdea573a19f250908b0ed1cb44d8b67f3fb29943
melquiadesmario <melquiadesm@gmail.com>
2021-06-03 23:14:12 -0300

    Created Directory with Routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Olá Dev!' });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log('Server started on port 3333!'));
